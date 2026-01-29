a3aef56710aa6a4ab1d53920eedffdd595690ee2
dlicheva <diana@ocurity.com>
2026-01-29 20:58:56 +0000

    day 6 part 1

--- advent_2025_day_6_part_1.py
+++ advent_2025_day_6_part_1.py
@@ -1,28 +1,39 @@
+def prod(factors):
+    p = 1
+    for i in factors:
+        p *= i
+    return p
+
+
+def rotate_90_degrees(matrix):
+    rotated_matrix = []
+    for i, horNumber in enumerate(matrix[0]):
+        newRow = []
+        for j, vertNumber in enumerate(matrix):
+            newRow.insert(0, matrix[j][i])
+        rotated_matrix.append(newRow)
+
+    return rotated_matrix
+
+
 def getGrandTotal(rows: list[str]) -> int:
-    a = 0
-    # split each row into an array of numbers
-    # save all the arrays
+    total = 0
     arrayed = []
     for val in rows:
         arrayOfVals = val.split()
         arrayed.append(arrayOfVals)
 
-    print(arrayed)
+    arrayed = rotate_90_degrees(arrayed)
 
-    for i, number in enumerate(arrayed[0]):
-        operation = arrayed[-1][i]
-        totalForCol = 0
-        if(operation == '+'):
-            for param in arrayed:
+    for row in arrayed:
+        factors = [int(x) for x in row[1:]]
+        print(row)
+        if row[0] == "+":
+            total += sum(factors)
+        elif row[0] == "*":
+            total += prod(factors)
 
-
-    # for each item in the first row
-    # find the operation
-    # find all other params
-    # get the result
-    # add it to the total
-
-    return a
+    return total
 
 
 def test_answer():
@@ -35,16 +46,8 @@ def test_answer():
     assert getGrandTotal(problems) == 4277556
 
 
-problems = [
-    "123 328  51 64",
-    "45 64  387 23",
-    "6 98  215 314",
-    "*   +   *   +  ",
-]
-getGrandTotal(problems)
-#
-# with open("day-6.txt", "r", encoding="utf-8") as f:
-#     problems = f.read().splitlines()
-#
-# all = getGrandTotal(problems)
-# print(all)
+with open("day-6.txt", "r", encoding="utf-8") as f:
+    problems = f.read().splitlines()
+
+all = getGrandTotal(problems)
+print(all)
